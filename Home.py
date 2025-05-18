ae19588f43f1513969a3895adc555e4daa61fb1e
dlempa <dl89ai@gmail.com>
2025-05-18 13:09:41 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -9,3 +9,14 @@ st.image("https://upload.wikimedia.org/wikipedia/en/6/6d/Philadelphia_Phillies_l
 
 
 st.sidebar.success("Select a page above.")
+
+with st.expander("⚠️ Data Disclaimer"):
+    st.markdown(
+        """
+        This app uses data obtained via the [unofficial MLB Stats API](https://statsapi.mlb.com/api/), 
+        accessed using the open-source [`mlb-statsapi`](https://github.com/toddrob99/MLB-StatsAPI) Python wrapper.
+
+        This project is not affiliated with or endorsed by Major League Baseball or its partners.
+        All data is intended for educational and non-commercial purposes.
+        """
+    )
