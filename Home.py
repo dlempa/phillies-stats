0007b8a3d777ae7c191b19021d9440ba139b9da8
dlempa <dl89ai@gmail.com>
2025-05-18 12:55:54 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,7 +7,5 @@ st.markdown("Welcome to the Phillies Stats App! Navigate through the sidebar to 
 
 st.image("https://upload.wikimedia.org/wikipedia/en/6/6d/Philadelphia_Phillies_logo.png", width=200)
 
-st.markdown("### 📊 Available Pages")
-st.markdown("- [Home Run Data](./homerundata)")
 
 st.sidebar.success("Select a page above.")
