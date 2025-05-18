c0d87313dcf82e714e3f42f121264da89e938be8
dlempa <dl89ai@gmail.com>
2025-05-18 13:23:52 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,13 +2,13 @@ import streamlit as st
 
 st.set_page_config(page_title="Phillies Stats", page_icon="assets/primary_logo_on_white_color.png", layout="centered")
 
-col1, col2 = st.columns([1, 4])  # adjust width ratio as needed
+col1, col2 = st.columns([2, 3])  # adjust width ratio as needed
 
 with col1:
     st.image("assets/primary_logo_on_white_color.png", width=50)
 
 with col2:
-    st.title("⚾ Phillies Stats Dashboard")
+    st.title("Phillies Stats Dashboard")
 
 st.markdown("Welcome to the Phillies Stats App! Navigate through the sidebar to explore different statistics.")
 
